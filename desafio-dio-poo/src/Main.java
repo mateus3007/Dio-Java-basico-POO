0319202b0be91c2dd53c8319dc21ce384c03b4fe
Mateus3007 <mateus3007araujo@gmail.com>
2023-02-26 00:06:53 -0300

    .

---
+++ desafio-dio-poo/src/Main.java
@@ -0,0 +1,8 @@
+import dominio.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+        System.out.println(curso);
+    }
+}
